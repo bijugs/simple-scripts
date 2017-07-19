fd9ab43abaff04b502e45362c6eaddee3c168129
Biju Nair <gs.biju@gmail.com>
2017-07-19 17:57:29 -0400

    Changes to the Batch put code to benchmark

--- hbase-samples/src/main/java/com/ssamples/hbase/BatchPutData.java
+++ hbase-samples/src/main/java/com/ssamples/hbase/BatchPutData.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.KeyValue;
 import org.apache.hadoop.hbase.TableName;
@@ -22,21 +23,44 @@ public class BatchPutData {
 		int numRows = 100;
 		int commitSize = 50;
 		int rows = 0;
-		Configuration conf = HBaseConfiguration.create();
-		conf.set("hbase.zookeeper.quorum", "localhost");
-		// Creating a connection
-		Connection conn = ConnectionFactory.createConnection(conf);
+                String zkQuorum = "localhost";
+                String principal = null;
+                String keyTab = null;
+                boolean isSecure = false;
+		Configuration config = HBaseConfiguration.create();
+                for (int i = 0; i < args.length; i++) {
+                        if (i == 0)
+                                zkQuorum = args[i];
+                        if (i == 1)
+                                numRows = Integer.parseInt(args[i]);
+                        if (i == 2)
+                                commitSize = Integer.parseInt(args[i]);
+                        if (i == 3)
+                                valueSize = Integer.parseInt(args[i]);
+                        if (i == 4) {
+                                if (args[i].equalsIgnoreCase("secure"))
+                                    isSecure = true;
+                        }
+                        if (i == 5)
+                                principal = args[i];
+                        if (i == 6)
+                                keyTab = args[i];
+
+                }
+                config.set("hbase.zookeeper.quorum", zkQuorum);
+                System.out.println("Hbase zookeeper "+config.get("hbase.zookeeper.quorum"));
+                if (isSecure) {
+                    config.set("hadoop.security.authentication", "Kerberos");
+                    config.set("hbase.security.authentication", "Kerberos");
+                    config.set("hbase.master.kerberos.principal", "hbase/_HOST@ADDEV.BLOOMBERG.COM");
+                    config.set("hbase.regionserver.kerberos.principal", "hbase/_HOST@ADDEV.BLOOMBERG.COM");
+                    UserGroupInformation.setConfiguration(config);
+                    if (principal != null && keyTab != null)
+                       UserGroupInformation.loginUserFromKeytab(principal,keyTab);
+                }
+		Connection conn = ConnectionFactory.createConnection(config);
 		TableName tblName = TableName.valueOf("t1"); 
 		Table tbl = conn.getTable(tblName);
-		for (int i = 0; i < args.length; i++) {
-			if (i == 0)
-				numRows = Integer.parseInt(args[0]);
-			if (i == 1)
-				commitSize = Integer.parseInt(args[1]);
-			if (i == 2)
-				valueSize = Integer.parseInt(args[2]);
-
-		}
 		ArrayList<Long>  stats = new ArrayList<Long>(numRows/commitSize);
 		StringBuffer value = new StringBuffer();
 		for (int i = 0; i < valueSize; i++) {
--- phoenix-samples/src/main/java/com/ssamples/phoenix/PhoenixPreparedStmt.java
+++ phoenix-samples/src/main/java/com/ssamples/phoenix/PhoenixPreparedStmt.java
@@ -1,5 +1,6 @@
 package com.ssamples.phoenix;
 
+import org.apache.log4j.Logger;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -7,9 +8,12 @@ import java.util.ArrayList;
 import java.util.Properties;
 
 public class PhoenixPreparedStmt {
+        
+        static Logger log = Logger.getLogger(PhoenixPreparedStmt.class.getName());
 	
 	public static void main(String args[]) throws Exception {
-		final String sql = "upsert into TBL values (?,?)";
+                log.debug("Log4j is enabled");
+		final String sql = "upsert into TBN values (?,?)";
 		int rows = 0;
 		String key = null;
 		StringBuffer value = new StringBuffer();
@@ -17,27 +21,53 @@ public class PhoenixPreparedStmt {
 		int valueSize = 10;
 		int numRows = 100;
 		int commitSize = 50;
+                boolean isSecure = false;
+                String principal = null;
+                String keyTab = null;
+                String zkQuorum = "localhost";
+                boolean autoCommit = false;
 		Properties prop = new Properties();
-		prop.setProperty("zookeeper.znode.parent", "hbase");
+		prop.setProperty("phoenix.mutate.batchSize", "5000");
 		Class.forName("org.apache.phoenix.jdbc.PhoenixDriver");
-		System.out.println("getting connection");
+                Connection con = null;
 		PreparedStatement stmt = null;
-		Connection con = DriverManager.getConnection("jdbc:phoenix:localhost:2181:/hbase");
-		if (!con.getAutoCommit()) {
-			System.out.println("Auto commit is off");
-			con.setAutoCommit(true);
-		}
-		System.out.println("connected: " + con.toString());
-		stmt = con.prepareStatement(sql);
 		for (int i = 0; i < args.length; i++) {
 			if (i == 0)
-				numRows = Integer.parseInt(args[0]);
-			if (i == 1)
-				commitSize = Integer.parseInt(args[1]);
+				zkQuorum = args[i];
+			if (i == 1) {
+                                if (args[i].equalsIgnoreCase("true"))
+				    autoCommit = true;
+                        }
 			if (i == 2)
-				valueSize = Integer.parseInt(args[2]);
+				numRows = Integer.parseInt(args[i]);
+			if (i == 3)
+				commitSize = Integer.parseInt(args[i]);
+			if (i == 4)
+				valueSize = Integer.parseInt(args[i]);
+			if (i == 5) {
+				if (args[i].equalsIgnoreCase("secure"))
+                                    isSecure = true;
+                        }
+			if (i == 6)
+				principal = args[i];
+			if (i == 7)
+				keyTab = args[i];
 
 		}
+		System.out.println("getting connection zkQuorum " +zkQuorum);
+                if (isSecure && (principal != null) && (keyTab != null))
+		    con = DriverManager.getConnection("jdbc:phoenix:"+zkQuorum+":2181:/hbase:"+principal+":"+keyTab,prop);
+                else
+		    con = DriverManager.getConnection("jdbc:phoenix:"+zkQuorum+":2181:/hbase",prop);
+		if (autoCommit) {
+			System.out.println("Auto commit is set");
+			con.setAutoCommit(true);
+		} else {
+			System.out.println("Auto commit is false");
+			con.setAutoCommit(false);
+                }
+		System.out.println("connected: " + con.toString());
+		stmt = con.prepareStatement(sql);
 		stats = new ArrayList<Long>(numRows/commitSize);
 		for (int i = 0; i < valueSize; i++) {
 			value = value.append('a');
@@ -57,8 +87,8 @@ public class PhoenixPreparedStmt {
 			stats.add(System.currentTimeMillis() - startTime);
 			System.out.println("Committed "+ (System.currentTimeMillis() - startTime));
 		}
-		System.out.println("Completed batch execute for "+numRows+" rows "+" commit size "+commitSize+" and val size "+valueSize);
-		System.out.println("Stats avg "+getAvg(stats)+" max "+getMax(stats)+" min "+getMin(stats));
+		System.out.println("Completed batch execute for "+numRows+" rows "+" commit size "+commitSize+" and val size "+valueSize+" autoCommit "+args[1]);
+		System.out.println("Stats avg "+getAvg(stats)+" max "+getMax(stats)+" min "+getMin(stats)+" Tot "+getTotal(stats));
 		Thread.sleep(6000);
 		stmt.close();
 		con.close();
@@ -88,5 +118,14 @@ public class PhoenixPreparedStmt {
 			out += in.get(i);
 		}
 		return out/in.size();
+	}
+
+
+	static long getTotal(ArrayList<Long> in) {
+		Long out = 0L;
+		for (int i = 0; i <= in.size() - 1; i++) {
+			out += in.get(i);
+		}
+		return out;
 	}
 }
