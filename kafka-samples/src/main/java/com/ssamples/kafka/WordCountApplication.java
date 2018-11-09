3835548e5a9dd8282154f104c042802bbb831c62
Biju Nair <gs.biju@gmail.com>
2018-11-08 21:02:45 -0500

    Add Kafka streams example

--- kafka-samples/src/main/java/com/ssamples/kafka/WordCountApplication.java
+++ kafka-samples/src/main/java/com/ssamples/kafka/WordCountApplication.java
@@ -19,7 +19,7 @@ public class WordCountApplication {
     public static void main(final String[] args) throws Exception {
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "Bijus-MacBook-Air.local:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
  
