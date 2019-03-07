b297f2bb7a2ef28a04397d7d7c1e96d12971fcad
Biju Nair <gs.biju@gmail.com>
2019-03-07 14:55:53 -0500

    Add test code for MultiUGI

--- hbase-samples/src/main/java/com/ssamples/hbase/HbaseUGI.java
+++ hbase-samples/src/main/java/com/ssamples/hbase/HbaseUGI.java
@@ -1,7 +1,6 @@
 package com.ssamples.hbase;
 
 import org.apache.hadoop.security.UserGroupInformation;
-import org.apache.hadoop.security.UserGroupInformation;
 import java.security.PrivilegedExceptionAction;
 import java.io.IOException;
 import java.util.Map;
@@ -15,12 +14,12 @@ import java.sql.Timestamp;
 import java.util.Date;
 import org.apache.hadoop.hbase.util.Bytes;
 
-public class HbaseMultiUGI implements Runnable {
+public class HbaseUGI implements Runnable {
 
   String threadName;
   Connection conn;
 
-  HbaseMultiUGI(String name, Connection conn) {
+  HbaseUGI(String name, Connection conn) {
     this.threadName = name;
     this.conn = conn;
   }
@@ -49,39 +48,32 @@ public class HbaseMultiUGI implements Runnable {
     try {
        for (int i = 0; i < args.length; i++)
           System.out.println(" arg "+i+" "+args[i]);
-       if (args.length < 5) {
-          System.out.println("Expected params : krbRealm, zkCluster1, zkCluster2, principal, keyTab");
-          return;
-       }
        String krbRealm = args[0];
        System.out.println("krbRealm = "+args[0]);
        Configuration config1 = HBaseConfiguration.create();
        config1.set("hbase.zookeeper.quorum", args[1]);
        System.out.println("Hbase zookeeper "+config1.get("hbase.zookeeper.quorum"));
        config1.set("hadoop.security.authentication", "Kerberos");
-       config1.set("hbase.security.authentication", "hbase/_HOST@"+krbRealm);
-       config1.set("hbase.master.kerberos.principal", "Kerberos");
-       config1.set("hbase.regionserver.kerberos.principal", "Kerberos");
-       MultiUserGroupInformation ugi1 = new MultiUserGroupInformation(config1, args[3], args[4]);
+       config1.set("hbase.security.authentication", "Kerberos");
+       config1.set("hbase.master.kerberos.principal", "hbase/_HOST@"+krbRealm);
+       config1.set("hbase.regionserver.kerberos.principal", "hbase/_HOST@"+krbRealm);
+       UserGroupInformation.setConfiguration(config1);
+       if (args.length == 4) {
+         UserGroupInformation.loginUserFromKeytab(args[2], args[3]);
+       }
        Connection conn1 = ConnectionFactory.createConnection(config1);
        System.out.println("************ Connected to HBase *************");
-       Thread t1 = new Thread(new HbaseMultiUGI("1",conn1));
+       /*Table table = conn1.getTable(TableName.valueOf("tbn"));
+       System.out.println("******* Done getTable ******");
+       HTableDescriptor desc = table.getTableDescriptor();
+       System.out.println("******* Done getTableDescriptor ******");
+       Get g = new Get(Bytes.toBytes("r1"));
+       Result r = table.get(g);
+       System.out.println("***** Completed table operation in thread "+new String(r.value()));
+       conn1.close();*/
+       Thread t1 = new Thread(new HbaseUGI("1",conn1));
        t1.start();
-
-//       Configuration config2 = HBaseConfiguration.create();
-//       config2.set("hbase.zookeeper.quorum", args[2]);
-//       System.out.println("Hbase zookeeper "+config2.get("hbase.zookeeper.quorum"));
-//       config2.set("hadoop.security.authentication", "Kerberos");
-//       config2.set("hbase.security.authentication", "hbase/_HOST@"+krbRealm);
-//       config2.set("hbase.master.kerberos.principal", "Kerberos");
-//       config2.set("hbase.regionserver.kerberos.principal", "Kerberos");
-//       MultiUserGroupInformation ugi2 = new MultiUserGroupInformation(config2, args[3], args[4]);
-//       Connection conn2 = ConnectionFactory.createConnection(config2);
-//       Thread t2 = new Thread(new HbaseMultiUGI("2",conn2));
-//       System.out.println("************ Connected to HBase *************");
-//       t2.start();
        t1.join();
-//       t2.join();
     } catch (Exception e) {
        e.printStackTrace();
     }
